64f97af4495d9fd8483638ba2c5e9983f6c3156b
Geovane Dias de Araujo <Diasgeovane7@gmail.com>
2024-06-24 01:23:35 -0300

    Melhorado o Hello Worl

--- LInuxTips/python_base/hello.py
+++ LInuxTips/python_base/hello.py
@@ -1,2 +1,34 @@
-#Este programa imprime Hello World
-print("Hello, World!")
+#! python3
+"""Hello World Multi Languages.
+
+Dependendo da língua configurada no ambiente o programa exibe a mensagem 
+correspondente.
+
+Como usar:
+
+TEnha a variável LANG devidamente configurada, ex:
+
+    export LANG = pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Geovane Dias de Araujo"
+__license__ = "Unlicence"
+
+import os
+
+current_language = "en_US"
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao,Mondo!"
+
+print(msg)
