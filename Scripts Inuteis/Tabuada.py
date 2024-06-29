1a044e6f6aeac7d5838a27b393ce717f37148a4a
GeovaneD7 <diasgeovane7@gmail.com>
2024-06-28 23:00:18 -0300

    Adicionado uma DocString e melhorado o código.

--- Scripts Inuteis/Tabuada.py
+++ Scripts Inuteis/Tabuada.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python
+"""Imprime uma tabuada do 1 ao 10."""
+__version__ = "0.1.2"
+__authon__ = "Geovane Dias"
+
 def tabuada():
-    n = int(input('Digite um número inteiro: '))
-    print(f'A tabuada de {n} é:')
+    n = int(input("Digite um número inteiro: "))
+    print(f"A tabuada de {n} é:")
     for i in range(1,11):
-        print(i*n)
+        print(f" {n} x {i} = ", i*n)
 
 tabuada()
