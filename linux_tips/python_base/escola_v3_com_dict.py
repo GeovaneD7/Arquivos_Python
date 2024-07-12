ea48a52477e0f97e5fcd27275579304ccfb015d8
GeovaneD7 <diasgeovane7@gmail.com>
2024-07-12 18:10:00 -0300

    Editado a v3 do script escola para fucionar com dicionários

--- linux_tips/python_base/escola_v3_com_dict.py
+++ linux_tips/python_base/escola_v3_com_dict.py
@@ -4,33 +4,33 @@
 -De cada sala da escola, quanas crianças estão matriculadas em cada atividade?
 Imprimir a lista de criança agrupadas por sala que frequenta cada uma das atividades.
 """
-__version__ = "0.1."
+__version__ = "0.1.3"
 __author__ = "Bruno Rocha"
 
 # Dados
-sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
-sala2 = [ "Joao", "Antonio", "Carlos", "Maria", "Isolda"]
+alunos = {
+"sala1": ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"],
+"sala2": [ "Joao", "Antonio", "Carlos", "Maria", "Isolda"]
+}
 
-aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
-aula_musica = ["Erik", "Carlos", "Maria"]
-aula_danca = ["Gustavo", "Sofia", "Antonio", "Joana"]
-
-atividades = [
-    ("Inglês", aula_ingles), 
-    ("Musica", aula_musica), 
-    ("Dança", aula_danca)
-]
+atividades = {
+    "Inglês": ["Erik", "Maia", "Joana", "Carlos", "Antonio"],
+    "Musica": ["Erik", "Carlos", "Maria"], 
+    "Dança": ["Gustavo", "Sofia", "Antonio", "Joana"]
+}
 
 # LIstar alunos em cada atividade por sala
+print()
+print("#" * 40)
 
-for nome_atividade, atividade in atividades:
+for atividade in atividades:
 
-    print(f"\nAlunos da atividade {nome_atividade}\n")
+    print(f"\nAlunos da atividade {atividade}\n")
     print("-" * 40)
     
-    # sala1 que tem interseção com a atividade
-    atividade_sala1 = set(sala1) & set(atividade)
-    atividade_sala2 = set(sala2).intersection(atividade)
+    # salas que tem interseção com a atividade
+    atividade_sala1 = set(alunos['sala1']) & set(atividades[atividade])
+    atividade_sala2 = set(alunos['sala2']) & set(atividades[atividade])
 
     print("sala 1: ", atividade_sala1)
     print("sala 2: ", atividade_sala2)
