f5b743ccaf1f17cfa3900407f2b64a561d378a90
GeovaneD7 <diasgeovane7@gmail.com>
2024-07-15 15:09:34 -0300

    Otimizado o hello.py usando dicionários

--- linux_tips/python_base/hello.py
+++ linux_tips/python_base/hello.py
@@ -16,23 +16,20 @@ Tenha a variavel LANG devidamente configurada, ex:
     ou
     ./hello.py
 """
-__version__ = "0.0.2"
-__author__ = "Geovane Dias de Araujo"
+__version__ = "0.1.2"
+__author__ = "Bruno Rocha"
 __license__ = "Unlicence"
 
 import os
 
 current_language = os.getenv("LANG", "en_US")[:5]
 
-msg = "Hello, World!"
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "es_SP": "Hola, Mundo!",
+    "fr_FR": "Bonjour, Monde!",
+}
 
-if current_language == "pt_BR":
-    msg = "Olá, Mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao,Mondo!"
-elif current_language == "es_SP":
-    msg = "Hola, Mundo!"
-elif current_language == "fr_FR":
-    msg = "Bonjour Monde"
-
-print(msg)                        
+print(msg[current_language])
